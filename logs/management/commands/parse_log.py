098ddb18b1cbb2c99507856a52d81e05c34e6672
miki <mikischeli@gmail.com>
2021-04-23 00:40:36 +0300

    update parse_log command

--- logs/management/commands/parse_log.py
+++ logs/management/commands/parse_log.py
@@ -3,7 +3,6 @@ import logging
 from datetime import datetime
 
 import requests
-from django.conf import settings
 from django.core.management import BaseCommand
 
 from logs.models import Log
@@ -21,36 +20,36 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         url = options['url'][0]
         logger.info(f"Start downloading {url}")
-        filename = self.download_file(url)
         res = {'total': 0, 'error': 0, 'success': 0}
-        logger.info(f"Start parsing {filename}")
-        with open(f'{settings.MEDIA_ROOT}logs/{filename}') as f:
-            for line in f:
-                if line == '\n':
-                    logger.error('Empty line')
-                    continue
-                if self.save_log(line):
-                    res['success'] += 1
-                else:
-                    res['error'] += 1
-                res['total'] += 1
+        logger.info(f"Start parsing {url}")
+        data = self.read_file(url)
+        for line in data:
+            if line.isspace() or not line:
+                logger.error('Empty line')
+                continue
+            try:
+                data = re.match(REGEX, line).groups()
+            except Exception as e:
+                logger.error(f'Error {e} on line: {line}')
+                continue
+            if self.save_log(data):
+                res['success'] += 1
+            else:
+                res['error'] += 1
+            res['total'] += 1
         logger.info(
-            f"Finish parsing {filename}. Total: {res['total']}, success: {res['success']}, errors: {res['error']}")
+            f"Finish parsing {url}. Total: {res['total']}, success: {res['success']}, errors: {res['error']}")
 
     @staticmethod
-    def download_file(url: str) -> str:
-        filename = f'{int(datetime.timestamp(datetime.now()))}_{url.split("/")[-1]}'
+    def read_file(url: str) -> str:
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
-            with open(f'{settings.MEDIA_ROOT}logs/{filename}', 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-        return filename
+            for line in r.iter_lines():
+                yield line.decode()
 
     @staticmethod
-    def save_log(line: str) -> bool:
+    def save_log(data: tuple) -> bool:
         try:
-            data = re.match(REGEX, line).groups()
             log = Log()
             log.ip = data[0]
             log.date = datetime.strptime(data[1], "%d/%b/%Y:%H:%M:%S %z")
@@ -60,6 +59,6 @@ class Command(BaseCommand):
             log.size = int(data[4]) if data[4].isdigit() else None
             log.save()
         except Exception as e:
-            logger.error(f'Error {e} on line: {line}')
+            logger.error(f"Error {e} on saving: {''.join(data)}")
             return False
         return True
